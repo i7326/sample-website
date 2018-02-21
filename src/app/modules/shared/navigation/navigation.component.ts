4f531549c0ace698627f61a80c38e16e473deaaa
Muhammed Irfan Kamaludeen <i7326@live.com>
2018-02-22 00:13:24 +0530

    Nav changes

--- src/app/modules/shared/navigation/navigation.component.ts
+++ src/app/modules/shared/navigation/navigation.component.ts
@@ -6,6 +6,50 @@ import { Component, Output } from '@angular/core';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent {
-  
+  menuItems: any[] = [{
+      name: 'Works',
+      description: 'What we do',
+      child: [{
+        name:'test1',
+        route:'test1'
+      },{
+        name:'test2',
+        route:'test2'
+      },{
+        name:'test3',
+        route:'test3'
+      },{
+        name:'test4',
+        route:'test4'
+      },{
+        name:'test5',
+        route:'test5'
+      },{
+        name:'test6',
+        route:'test6'
+      }]
+  },{
+      name: 'Works',
+      description: 'What we do',
+      child: [{
+        name:'test1',
+        route:'test1'
+      },{
+        name:'test2',
+        route:'test2'
+      },{
+        name:'test3',
+        route:'test3'
+      },{
+        name:'test4',
+        route:'test4'
+      },{
+        name:'test5',
+        route:'test5'
+      },{
+        name:'test6',
+        route:'test6'
+      }]
+  }];
 
 }
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -5,16 +5,26 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { HeaderComponent } from './header/header.component'
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { NavigationService } from './service/navigation.service';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true
+};
 
 /**
  * Do not specify providers for modules that might be imported by a lazy loaded module.
  */
 
 @NgModule({
-  imports: [CommonModule, FlexLayoutModule],
+  imports: [CommonModule, FlexLayoutModule, PerfectScrollbarModule],
   declarations: [NavigationComponent, HeaderComponent],
   exports: [NavigationComponent, HeaderComponent],
-  providers: [NavigationService],
+  providers: [NavigationService,{
+      provide: PERFECT_SCROLLBAR_CONFIG,
+      useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
+    }],
 })
 export class SharedModule {
   static forRoot(): ModuleWithProviders {
